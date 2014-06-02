aa8c463d66b79d583993f3979e66fff8363ff470
Daan Debie <d.debie@thenewmotion.com>
2014-06-02 14:02:31 +0200

    Don't need git-friendly

--- install-deps.sh
+++ install-deps.sh
@@ -1,10 +1,5 @@
 # up to you (me) if you want to run this as a file or copy paste at your leisure
 
-
-# https://github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
 # homebrew!
 # you need the code CLI tools YOU FOOL.
 ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)

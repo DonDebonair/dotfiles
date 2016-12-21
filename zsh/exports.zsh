b785c23c695131d7d053bfe8a3e6cd671f77e276
Daan Debie <debie.daan@gmail.com>
2016-12-21 13:56:31 +0100

    Bring back exports for stuff that isn't used in $PATH

---
+++ zsh/exports.zsh
@@ -0,0 +1,16 @@
+# Give Maven plenty of memory
+export MAVEN_OPTS='-Xms512m -Xmx1024m'
+
+# Highlight section titles in manual pages
+export LESS_TERMCAP_md="$fg[yellow]"
+
+# Don’t clear the screen after quitting a manual page
+export MANPAGER="less -X"
+
+export WORKON_HOME=$HOME/.virtualenvs
+
+# Prefer US English and use UTF-8
+export LANG="en_US"
+export LC_ALL="en_US.UTF-8"
+
+export EDITOR="subl"

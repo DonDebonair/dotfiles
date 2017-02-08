6320aabd4ac07247e63f3d08315f85b5e080ca73
Daan Debie <debie.daan@gmail.com>
2017-02-08 16:02:26 +0100

    add a lot of new stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -139,7 +139,8 @@ alias uuid="uuidgen | tr -d '\n' | tr '[:upper:]' '[:lower:]'  | pbcopy && pbpas
 # Docker stuff
 alias dm=docker-machine
 alias dc=docker-compose
-alias dkill='docker rm -f $(docker ps -aq)'
+alias dkill='docker rm -f $(docker ps -q)'
+alias dkillall='docker rm -f $(docker ps -aq)'
 
 # Copy my public SSH key to clipboard
 alias cpk='cat ~/.ssh/id_rsa.pub | pbcopy; echo "Public key copied to clipboard"'

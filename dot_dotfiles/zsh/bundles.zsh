2e0352023653e3b2eab56c4ece4e78c226fa51e6
Daan Debie <daan@dv.email>
2021-04-27 21:52:24 +0200

    Add some more autocomplete

--- dot_dotfiles/zsh/bundles.zsh
+++ dot_dotfiles/zsh/bundles.zsh
@@ -11,12 +11,15 @@ if ! zgen saved; then
     zgen oh-my-zsh plugins/sudo
     zgen oh-my-zsh plugins/docker
     zgen oh-my-zsh plugins/kubectl
+    zgen oh-my-zsh plugins/aws
+    zgen oh-my-zsh plugins/fzf
     zgen oh-my-zsh themes/lambda
 
     # Load other plugins
     zgen load djui/alias-tips
     zgen load zsh-users/zsh-syntax-highlighting
     zgen load zsh-users/zsh-history-substring-search
+    zgen load zsh-users/zsh-completions src
 
     # Save it
     zgen save

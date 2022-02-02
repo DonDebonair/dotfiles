969131dfe0fc7ff2411d4eae58d51ee8ee0657b0
Daan Debie <daan@dv.email>
2022-02-02 23:11:24 +0100

    remove unneeded stuff

--- dot_dotfiles/zsh/bundles.zsh
+++ dot_dotfiles/zsh/bundles.zsh
@@ -10,7 +10,6 @@ if ! zgen saved; then
     zgen oh-my-zsh plugins/pip
     zgen oh-my-zsh plugins/sudo
     zgen oh-my-zsh plugins/docker
-    zgen oh-my-zsh plugins/kubectl
     zgen oh-my-zsh plugins/aws
     zgen oh-my-zsh plugins/fzf
     zgen oh-my-zsh themes/lambda
@@ -18,7 +17,6 @@ if ! zgen saved; then
     # Load other plugins
     zgen load djui/alias-tips
     zgen load zsh-users/zsh-syntax-highlighting
-    zgen load zsh-users/zsh-history-substring-search
     zgen load zsh-users/zsh-completions src
 
     # Save it
@@ -28,5 +26,3 @@ fi
 
 # history search with UP/DOWN arrows
 zmodload zsh/terminfo
-bindkey "$terminfo[cuu1]" history-substring-search-up
-bindkey "$terminfo[cud1]" history-substring-search-down

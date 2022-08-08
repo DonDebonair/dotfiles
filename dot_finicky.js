1553cd5df41b8618ece1e192da5d8248dfae4171
Daan Debie <daan@dv.email>
2022-08-08 17:53:14 +0200

    feat: add finicky support

---
+++ dot_finicky.js
@@ -0,0 +1,42 @@
+module.exports = {
+  defaultBrowser: "Google Chrome",
+  rewrite: [
+    {
+      // Redirect all urls to use https
+      match: ({ url }) => url.protocol === "http",
+      url: { protocol: "https" }
+    }
+  ],
+  handlers: [
+    {
+      match: [
+        "zoom.us/j/*",
+        finicky.matchDomains(/.*\.zoom.us\/j/),
+        /zoom.us\/j\//,
+      ],
+      browser: "us.zoom.xos"
+    },
+    {
+      match: /^https?:\/\/.*\.console\.aws\.amazon\.com\/.*$/,
+      browser: {
+        name: "Google Chrome",
+        profile: "Profile 1",
+      }
+    },
+    {
+      match: /^https?:\/\/www\.notion\.so\/.*$/,
+      browser: "Notion"
+    },
+    {
+      match: "open.spotify.com/*",
+      browser: "Spotify"
+    },
+    {
+      match: /^https:\/\/www\.google\.com\/calendar\/.*$/,
+      browser: {
+        name: "Google Chrome",
+        profile: "Profile 1",
+      }
+    }
+  ]
+}

aad9c5207f5c89dc3e105b4dd1055447201d8214
Daan Debie <daan@dv.email>
2024-10-02 19:32:35 +0200

    feat: remove Source.ag

--- dot_finicky.js
+++ dot_finicky.js
@@ -2,24 +2,8 @@ module.exports = {
   defaultBrowser: "Firefox",
   handlers: [
     {
-      match: [
-        "zoom.us/j/*",
-        finicky.matchDomains(/.*\.zoom.us\/j/),
-        /zoom.us\/j\//,
-      ],
-      browser: "us.zoom.xos"
-    },
-    {
-      match: /^https?:\/\/www\.notion\.so\/sourceagriculture\/.*$/,
-      browser: "Notion"
-    },
-    {
       match: "open.spotify.com/*",
       browser: "Spotify"
-    },
-    {
-      match: "miro.com/app/*",
-      browser: "Miro"
     }
   ]
 }

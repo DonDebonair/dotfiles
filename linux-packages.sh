518f532414b2f2e22d1f290eb0d614b2dff085ca
Daan Debie <daan@dv.email>
2025-08-31 18:49:07 +0200

    fix mac-only things

--- linux-packages.sh
+++ linux-packages.sh
@@ -7,10 +7,3 @@ package 'jq'
 package 'autojump'
 package 'fzf'
 package 'starship'
-
-# # Install direnv
-if ! command -v direnv &> /dev/null; then
-    curl -s https://api.github.com/repos/direnv/direnv/releases/latest \
-    | jq -r ".assets[] | select(.name | contains(\"linux-amd64\")) | .browser_download_url" \
-    | wget -O ~/bin/direnv -q -i - 1>/dev/null 2>&1 && chmod +x ~/bin/direnv
-fi
--- run_once_50_karabiner-bootstrap.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cp $(chezmoi source-path)/bootstrap/karabiner.json ~/.config/karabiner
